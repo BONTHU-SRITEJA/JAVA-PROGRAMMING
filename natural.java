a5aeb813acb14fd9b45ad7aa658423367f4af762
BONTHU-SRITEJA <23pa1a0535@vishnu.edu.in>
2024-08-01 15:54:06 +0530

    Add files via upload
    TO PRINT A TABLE OF FIRST 10 NATURAL NUMBERS ,THEIR SQUARES AND THEIR CUBES .

---
+++ natural.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class nat {
+    public static void main(String []args) {
+        Scanner in=new Scanner(System.in);
+        for(int i=1;i<10;i++)
+        {	
+        System.out.println(i+" "+i*i+" "+i*i*i);
+      
+	}
+}
+}	
